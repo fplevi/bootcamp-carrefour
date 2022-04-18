16329635be4eb8799b41cc1b869b791adeefa9c0
fplevi <fplevi@gmail.com>
2022-04-17 23:27:17 -0300

    Adiciona exercicios realizados

---
+++ primeiros-passos/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    
+  <style type="text/css">
+      * {
+          margin:0;
+          padding:0;
+          font-family: sans-serif;
+      }
+      .ui-widget-header {
+            background-color:#007fff !important;
+      }
+      header {
+          color:#007fff;
+          padding: 20px 0;
+          text-align:center;
+      }
+      #accordion, #tabs, header, #progressbar {
+          max-width:728px;
+          margin: 15px auto;
+      }
+  </style>
+    <title>Meu primeiro site em PHP</title>
+    
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+  $( function() {
+    for ($value = 0 ; $value < 50 ; $value++) {
+        $( "#progressbar" ).progressbar({
+      value: $value
+    });
+    }
+    
+  } );
+  </script>
+</head>
+<body>
+    <header>
+        <h1>Site de exemplos de apliação jQuery</h1>
+    </header>
+<div id="tabs">
+  <ul>
+    <li><a href="#tabs-1">Nunc tincidunt</a></li>
+    <li><a href="#tabs-2">Proin dolor</a></li>
+    <li><a href="#tabs-3">Aenean lacinia</a></li>
+  </ul>
+  <div id="tabs-1">
+    <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+  </div>
+  <div id="tabs-2">
+    <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+  </div>
+  <div id="tabs-3">
+    <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+    <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+  </div>
+</div>
+
+<div id="accordion">
+<?php 
+        for ($i = 0 ; $i < 4 ; $i++) {
+            print("<h3>Cabeçalho " . $i . "</h3>");
+            print("<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil veritatis reiciendis error a pariatur, recusandae consequuntur ex repudiandae iste aspernatur aliquid, iusto dolore doloremque! Inventore dolorem cum magni reprehenderit eveniet. </p>");
+        }
+    ?>
+</div>
+
+<div id="progressbar"></div>
+
+</body>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                alert("Site carregado!") ;
+            }) ;
+        </script>
+</html>
